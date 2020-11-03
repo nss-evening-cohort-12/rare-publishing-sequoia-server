f02515b8ee56cb794a772040b12cd01047ae0ef4
Matt Logan <mlogan5212@gmail.com>
2020-11-02 20:48:49 -0600

    created get all posts method

--- posts/request.py
+++ posts/request.py
@@ -19,3 +19,29 @@ def create_post(new_post):
         new_post['id'] = id
 
     return json.dumps(new_post)
+
+
+def get_all_posts():
+    with sqlite3.connect("./rare.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor
+
+        db_cursor.execute("""
+            p.id,
+            p.user_id,
+            p.category_id,
+            p.title,
+            p.content,
+            p.publication_date,
+            p.header_img
+        FROM Posts p
+        """)
+
+        posts = []
+        dataset = db_cursor.fetchall()
+        for row in dataset:
+            post = Post(row['id'], row['user_id'], row['category_id'], row['title'], row['content'], row['publication_date'], row['header_img'])
+
+            posts.append(post.__dict__)
+    
+    return json.dumps(posts)
